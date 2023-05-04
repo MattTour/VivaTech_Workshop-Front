a5a99755d33df4908afb1ff430a86ba08accc020
Pepito94510 <kevin.boissiere94@gmail.com>
2023-05-04 16:28:50 +0200

    Add Home page

--- lib/main.dart
+++ lib/main.dart
@@ -17,8 +17,9 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'VivaTechApp',
         theme: ThemeData(
+          fontFamily: 'MuseoSans',
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         ),
         home: MyHomePage(),
       ),
@@ -114,43 +115,33 @@ class _MyHomePageState extends State<MyHomePage> {
 class GeneratorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var pair = appState.current;
-
-    IconData icon;
-    if (appState.favorites.contains(pair)) {
-      icon = Icons.favorite;
-    } else {
-      icon = Icons.favorite_border;
-    }
-
     return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          BigCard(pair: pair),
-          SizedBox(height: 10),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ElevatedButton.icon(
-                onPressed: () {
-                  appState.toggleFavorite();
-                },
-                icon: Icon(icon),
-                label: Text('Like'),
-              ),
-              SizedBox(width: 10),
-              ElevatedButton(
-                onPressed: () {
-                  appState.getNext();
-                },
-                child: Text('Next'),
-              ),
-            ],
-          ),
-        ],
-      ),
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/Logo_vivatech.png'),
+            Text("Viva Technology is Europe's biggest startup and tech event. The seventh edition will take place June 14-17, 2023, in Paris, at Expo Porte de Versailles.",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontWeight: FontWeight.w700),
+            ),
+            Image.asset('assets/lead-the-future.jpg'),
+            Text("VivaTech accelerates innovation by connecting startups, tech leaders, major corporations and investors responding to our world’s biggest challenges.  This is where business meets innovation.",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontWeight: FontWeight.w500),
+            ),
+            Image.asset('assets/startup-paradise.jpg'),
+            Text("VivaTech organizes Europe’s biggest technology and startup event each year, over four exciting days in Paris.  We unite the most disruptive topics in tech, rule-breaking entrepreneurs, and the world’s largest technology breakthroughs, building an ecosystem where business gets done.",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontWeight: FontWeight.w500),
+            ),
+            Text("VivaTech is a global community including thousands of visionary startups, investors, organizations, corporations, researchers, media, and talent.",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontWeight: FontWeight.w500),
+            ),
+          ],
+        ),
+      )
     );
   }
 }
